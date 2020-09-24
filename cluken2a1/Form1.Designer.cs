e02b693d5135abdec854a2bb0b289d0cb115ad62
Callum Luken <gm4134oe@go.minnstate.edu>
2020-09-24 11:51:56 -0500

    Completed exercise 2a1

---
+++ cluken2a1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace cluken2a1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCaluclate = new System.Windows.Forms.Button();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDiscountAmount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDiscountPercent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(154, 110);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 24;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+            // 
+            // btnCaluclate
+            // 
+            this.btnCaluclate.Location = new System.Drawing.Point(40, 110);
+            this.btnCaluclate.Name = "btnCaluclate";
+            this.btnCaluclate.Size = new System.Drawing.Size(75, 23);
+            this.btnCaluclate.TabIndex = 22;
+            this.btnCaluclate.Text = "&Calculate";
+            this.btnCaluclate.UseVisualStyleBackColor = true;
+            this.btnCaluclate.Click += new System.EventHandler(this.btnCaluclate_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(110, 84);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(119, 20);
+            this.txtTotal.TabIndex = 27;
+            this.txtTotal.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Total:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtDiscountAmount
+            // 
+            this.txtDiscountAmount.Location = new System.Drawing.Point(110, 58);
+            this.txtDiscountAmount.Name = "txtDiscountAmount";
+            this.txtDiscountAmount.ReadOnly = true;
+            this.txtDiscountAmount.Size = new System.Drawing.Size(119, 20);
+            this.txtDiscountAmount.TabIndex = 25;
+            this.txtDiscountAmount.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Discount amount:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtDiscountPercent
+            // 
+            this.txtDiscountPercent.Location = new System.Drawing.Point(110, 32);
+            this.txtDiscountPercent.Name = "txtDiscountPercent";
+            this.txtDiscountPercent.Size = new System.Drawing.Size(119, 20);
+            this.txtDiscountPercent.TabIndex = 21;
+            this.txtDiscountPercent.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Discount percent:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(110, 6);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.Size = new System.Drawing.Size(119, 20);
+            this.txtSubtotal.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "&Subtotal:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 261);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCaluclate);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDiscountAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDiscountPercent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "cluken2a1 Variables";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCaluclate;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDiscountAmount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDiscountPercent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ cluken2a1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cluken2a1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCaluclate_Click(object sender, EventArgs e)
+        {
+            //txtDiscountAmount.Text  +
+            //    (Convert.ToDecimal(txtSubtotal.Text)
+            //   * Convert.ToDecimal(txtDiscountPercent.Text) / 100).ToString("0.00");
+
+            decimal subTotal = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discountPercent = Convert.ToDecimal(txtDiscountPercent.Text);
+            decimal discountAmount = subTotal * discountPercent / 100;
+            txtDiscountAmount.Text = discountAmount.ToString("0.00");
+
+
+            //txtTotal.Text =
+            //    (Convert.ToDecimal(txtSubtotal.Text)
+            //   - Convert.ToDecimal(txtDiscountAmount.Text)).ToString("0.00");
+
+            decimal Total = subTotal - discountAmount;
+            txtTotal.Text = Total.ToString("0.00");
+        }
+
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
