b1e59eac56fcb26b1608c45e36bf3fbf06a0f88a
Callum Luken <gm4134oe@go.minnstate.edu>
2020-12-10 01:01:45 -0600

    Completed Exercise 3d

---
+++ cluken3d1/Ex3dCalculations.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cluken3d1
+{
+    public class Ex3dCalculations
+    {
+        public static string ArrayToString(int[] values)
+        {
+            string result = "";
+            foreach (int value in values)
+            {
+                result += value.ToString() + ", ";
+            }
+            // Create StringBuilder object: sbResult
+            // loop
+            //     convert current value to string, append to sbResult
+            //     append ", " to sbResult
+            // convert sbResult to string: result
+            // remove last ", " from result
+            // return result
+            return result.Remove(result.LastIndexOf(", "));
+        }
+
+        public static int ValueCount(int[] values, int searchValue)
+        {
+            // create counter variable
+            // loop
+            //     increment counter if current value equals searchValue
+            // return counter
+            int count = 0;
+
+                foreach (int value in values)
+                {
+                    if (searchValue == value)
+                    count++;
+                }
+
+                return count;
+        }
+
+        public static int RangeCount(int[] values, int searchMin, int searchMax)
+        {
+            // create counter variable
+            // loop
+            //     increment counter if current value between min and max (inclusive)
+            // return counter
+            int count = 0;
+
+            foreach (int value in values)
+            {
+                if (value >= searchMin && value <= searchMax)
+                    count++;
+            }
+                return count;
+        }
+    }
+}
---
+++ cluken3d1UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("cluken3d1UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cluken3d1UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4dee5ce5-bef1-47e5-a3e5-4125d6c464aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
